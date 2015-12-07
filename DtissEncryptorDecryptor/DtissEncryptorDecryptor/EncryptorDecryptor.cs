1810df92359135ad79893dee752f057e6fcb3645
asreerangan <anoop.rangan@gmail.com>
2015-12-07 11:58:03 +0000

    adding EncryptorDecryptor

---
+++ DtissEncryptorDecryptor/DtissEncryptorDecryptor/EncryptorDecryptor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.Configuration;
+using System.Xml;
+
+namespace DtissEncryptorDecryptor
+{
+    public class EncryptorDecryptor
+    {
+
+        public static string StringtoEncrypt(string value, bool useHash)
+        {
+
+            byte[] keyArray;
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(value);
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(@"D:\Dtiss\dtissconfig.xml");
+
+            string key = xmldoc.SelectSingleNode("//SecurityKey").InnerText;
+
+            if (useHash)
+            {
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                hashmd5.Clear();
+            }
+            else
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            tdes.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+           
+        
+        
+        
+        
+        }
+
+        public static string StringtoDecrypt(string value, bool useHash)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = Convert.FromBase64String(value);
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(@"D:\Dtiss\dtissconfig.xml");
+
+            string key = xmldoc.SelectSingleNode("//SecurityKey").InnerText;
+
+            if (useHash)
+            {
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                hashmd5.Clear();
+            }
+            else
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            tdes.Clear();
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
